a2e31939dc0153229e50aca8006db73fa1a34cc9
zhou <2251073096@qq.com>
2021-07-11 22:41:47 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+		System.out.println("查看状态 git status");
+	}	
+}
