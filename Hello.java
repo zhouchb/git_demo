087ac689cfe2850b1333a7453828ba25ee691b83
zhou <2251073096@qq.com>
2021-07-12 09:54:19 +0800

    first commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("查看状态 git status");
+		System.out.println("第一次修改！！！！！");
 		System.out.println("查看状态 git status");
 		System.out.println("查看状态 git status");
 		System.out.println("查看状态 git status");
